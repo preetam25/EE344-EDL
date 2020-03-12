254bbcc0babc6266441a3a6a5bdfee08c8c8b9c1
ameyanjarlekar <40833548+ameyanjarlekar@users.noreply.github.com>
2020-03-12 14:26:00 +0530

    Update 1d_estimate.py

--- 1d_estimate.py
+++ 1d_estimate.py
@@ -3,25 +3,32 @@ import math
 import random
 import matplotlib.pyplot as plt
 from scipy import stats
+
+# declare number of particles used for object track estimation
 particles = 100
-likelihood = np.empty(particles)
-estimated = np.empty(observations)
+
+# declare arrays
+likelihood = np.empty(particles)   # calculate likelihood of estimate provided by the particle position
+estimated = np.empty(observations) # stores estimated path of the particle
+
+# initial particle position
 particle_estimate = np.random.uniform(-0.5,1,(particles))
+
+# particle filter 
 i = 0
 while i <observations:
-  particle_estimate = particle_estimate + np.random.normal(0,5*sigmax,(particles))
+  particle_estimate = particle_estimate + np.random.normal(0,5*sigmax,(particles))            # perturb previous particle position for fresh estimate
   j = 0
   while j < np.shape(particle_estimate)[0]:
-    likelihood[j] = math.exp(-5*((m[i]-ay*math.exp(particle_estimate[j]))**2))
+    likelihood[j] = math.exp(-5*((m[i]-ax*math.exp(particle_estimate[j]))**2))                # calculate likelihood based on estimated particle position and observation
     j = j+1
-  likelihood = likelihood/np.sum(likelihood)
-  custm = stats.rv_discrete(name='custm', values=(particle_estimate*10000000, likelihood))
-  particle_estimate = custm.rvs(size=particles)/10000000
-  #print("actual",x[i])
-  #print("estimated",np.mean(particle_estimate))
-  estimated[i] = np.mean(particle_estimate)
-  #print("error",(x[i]-np.mean(particle_estimate))/x[i])
+  likelihood = likelihood/np.sum(likelihood)                                                  # normalize likelihood
+  custm = stats.rv_discrete(name='custm', values=(particle_estimate*10000000, likelihood))    # generate distribution from likelihood
+  particle_estimate = custm.rvs(size=particles)/10000000                                      # resample particles using generated likelihood
+  estimated[i] = np.mean(particle_estimate)                                                   # estimate particle location
   i= i+1
-plt.plot(x)
-plt.plot(estimated)
+
+# plotting
+plt.plot(x)          # original position
+plt.plot(estimated)  # estimated position
 plt.show()
