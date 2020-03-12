14c09ef901b8f0b6c5fb8ddb95870e56da58cbde
ameyanjarlekar <40833548+ameyanjarlekar@users.noreply.github.com>
2020-03-12 14:25:13 +0530

    Update 1d_data_generate.py

--- 1d_data_generate.py
+++ 1d_data_generate.py
@@ -2,22 +2,34 @@ import numpy as np
 import math
 import random
 import matplotlib.pyplot as plt
-sigmax = 0.02
-sigmam = 0.02
-i = 1
+
+sigmax = 0.02  # to model random perturbations in the object track
+sigmam = 0.02  # to model noise in observations
+
 observations = 500
-x = np.empty(observations)
-m = np.empty(observations)
+
+# declare arrays
+x = np.empty(observations)  # x coordinate of the object
+m = np.empty(observations)  # observation corresponsind to object position
+
+# path parameters 
 ax =1.0
 am = 0.4
-x[0] = 0.5
-m[0] = ay*math.exp(x[0]) + random.gauss(0,sigmam)
+
+# initial values
+x[0] = 0
+m[0] = ax*math.exp(x[0]) + random.gauss(0,sigmam)
+
+i = 1
 while i<observations:
-  x[i] = ax*x[i-1] + random.gauss(0,sigmax)
-  m[i] = ay*math.exp(x[i]) + random.gauss(0,sigmam)
+  x[i] = ax*x[i-1] + random.gauss(0,sigmax)          # particle moves linearly with perturbations
+  m[i] = ax*math.exp(x[i]) + random.gauss(0,sigmam)  # noisy observations are generated wrt each particle position
   i = i+1
 
-plt.plot(m)
-plt.show()
+# plot x coordinate wrt time
 plt.plot(x)
+plt.show()
+
+# plot observed values wrt time
+plt.plot(m)
 plt.show()
