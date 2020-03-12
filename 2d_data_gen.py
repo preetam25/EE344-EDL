cc2be3ba962524a2c278a4d834cb36987e68435d
ameyanjarlekar <40833548+ameyanjarlekar@users.noreply.github.com>
2020-03-12 14:26:19 +0530

    Update 2d_data_gen.py

--- 2d_data_gen.py
+++ 2d_data_gen.py
@@ -18,14 +18,14 @@ ax =1.0
 ay =1.0
 am1 = 0.4
 am2 = 0.4
-x[0] = 0.5
-y[0] = 0.5
+x[0] = 0.0
+y[0] = 0.0
 m1[0] = ay*math.exp(xscale*x[0]+y[0]) + random.gauss(0,sigmam1)
 m2[0] = ay*math.exp(x[0]+yscale*y[0]) + random.gauss(0,sigmam2)
 
 while i<observations:
-  x[i] = ax*x[i-1] + random.gauss(0,sigmax)
-  y[i] = ay*y[i-1] + random.gauss(0,sigmay)
+  x[i] = 0.01*i 
+  y[i] = ay*(x[i])**2 + random.gauss(0,sigmay)
   m1[i] = ay*math.exp(xscale*x[i]+y[i]) + random.gauss(0,sigmam1)
   m2[i] = ay*math.exp(x[i]+yscale*y[i]) + random.gauss(0,sigmam2)
 
@@ -33,7 +33,7 @@ while i<observations:
 
 plt.plot(x)
 plt.show()
-plt.plot(x)
+plt.plot(y)
 plt.show()
 plt.plot(x,y)
 plt.show()
