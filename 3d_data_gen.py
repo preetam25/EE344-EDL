7fb3140df84700e0e4279da2c07ed5c642ea5128
ameyanjarlekar <40833548+ameyanjarlekar@users.noreply.github.com>
2020-03-12 15:36:00 +0530

    Update 3d_data_gen.py

--- 3d_data_gen.py
+++ 3d_data_gen.py
@@ -2,32 +2,38 @@ import numpy as np
 import math
 import random
 import matplotlib.pyplot as plt
-sigmax = 0.02
-sigmay = 0.02
-sigmaz = 0.02
-xscale = 2
-yscale = 3
-zscale = 4
-sigmam1 = 0.02
-sigmam2 = 0.02
-sigmam3 = 0.02
-i = 1
-observations = 100
-x = np.empty(observations)
-y = np.empty(observations)
-z = np.empty(observations)
-m1 = np.empty(observations)
-m2 = np.empty(observations)
-m3 = np.empty(observations)
+from mpl_toolkits.mplot3d import Axes3D
+
+# scaling constants
+sigmax = 0.02      # to model random perturbations in the xcoordinate of object track 
+sigmay = 0.02      # to model random perturbations in the ycoordinate of object track 
+sigmaz = 0.02      # to model random perturbations in the zcoordinate of object track 
+xscale = 2         # constants for generating observation
+yscale = 3         # constants for generating observation
+zscale = 4         # constants for generating observation
+sigmam1 = 0.03     # to model noise in observation1
+sigmam2 = 0.04     # to model noise in observation2
+sigmam3 = 0.02     # to model noise in observation3
+
+observations = 100           # total number of states
+
+# declare arrays
+x = np.empty(observations)    # actual x coordinate of the particle
+y = np.empty(observations)    # actual y coordinate of the particle
+z = np.empty(observations)    # actual z coordinate of the particle
+m1 = np.empty(observations)   # stores observation1
+m2 = np.empty(observations)   # stores observation2
+m3 = np.empty(observations)   # stores observation3
+
+# constants for generating data
 ax =1.0
-ay =1.0
-az =1.0
-mx =1.0
-my =1.0
-mz =1.0
-am1 = 0.4
-am2 = 0.4
-am3 = 0.4
+ay =0.5
+az =0.7
+mx =1.2
+my =1.1
+mz =1.4
+
+# initial conditions
 x[0] = 0.0
 y[0] = 0.0
 z[0] = 0.0
@@ -35,24 +41,32 @@ m1[0] = mx*math.exp(xscale*x[0]+yscale*y[0]+z[0]) + random.gauss(0,sigmam1)
 m2[0] = my*math.exp(xscale*x[0]+y[0]+zscale*z[0]) + random.gauss(0,sigmam2)
 m3[0] = mz*math.exp(x[0]+yscale*y[0]+zscale*z[0]) + random.gauss(0,sigmam3)
 
+i = 1
 while i<observations:
-  x[i] = 0.01*i
-  y[i] = ay*(x[i]**2) + random.gauss(0,sigmay)
-  z[i] = az*z[i-1] + random.gauss(0,sigmaz)
-  m1[i] = mx*math.exp(xscale*x[i]+yscale*y[i]+z[i]) + random.gauss(0,sigmam1)
-  m2[i] = my*math.exp(xscale*x[i]+y[i]+zscale*z[i]) + random.gauss(0,sigmam2)
-  m3[i] = mz*math.exp(x[i]+yscale*y[i]+zscale*z[i]) + random.gauss(0,sigmam3)
+  x[i] = 0.01*i                                                                  # generate data (xcoordinate)
+  y[i] = ay*(x[i]**2) + random.gauss(0,sigmay)                                   # generate data (ycoordinate) 
+  z[i] = az*z[i-1] + random.gauss(0,sigmaz)                                      # generate data (zcoordinate)
+  m1[i] = mx*math.exp(xscale*x[i]+yscale*y[i]+z[i]) + random.gauss(0,sigmam1)    # generate observation1
+  m2[i] = my*math.exp(xscale*x[i]+y[i]+zscale*z[i]) + random.gauss(0,sigmam2)    # generate observation2
+  m3[i] = mz*math.exp(x[i]+yscale*y[i]+zscale*z[i]) + random.gauss(0,sigmam3)    # generate observation3
   i = i+1
 
-plt.plot(x)
+# plotting
+plt.plot(x)     # plot actual x coordinate of the particle wrt time
 plt.show()
-plt.plot(y)
+plt.plot(y)     # plot actual y coordinate of the particle wrt time
 plt.show()
-plt.plot(z)
+plt.plot(z)     # plot actual z coordinate of the particle wrt time
 plt.show()
-plt.plot(x,y)
+plt.plot(x,y)   # plot actual path in x-y
 plt.show()
-plt.plot(y,z)
+plt.plot(y,z)   # plot actual path in y-z
 plt.show()
-plt.plot(x,z)
+plt.plot(x,z)   # plot actual path in x-z
+plt.show()
+
+# show the actual path of the particle in 3D
+fig = plt.figure()
+ax = plt.axes(projection="3d")
+ax.plot3D(x, y, z, 'blue')
 plt.show()
